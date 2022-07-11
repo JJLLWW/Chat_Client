1677f8c814667356bc93c063e0510f5ff020abd4
Jack Wright <jack.louie.wright@gmail.com>
2022-07-11 14:00:13 +0100

    ignore dist, prettier working

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import {Routes, Route} from 'react-router-dom';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 export default function AppRoutes() {
     return (
-    <Routes>
-          <Route path="/" element={<h1>Main Page</h1>} />
-        <Route path="/login" element={<h1>Login</h1>} />
-    </Routes>
+        <Routes>
+            <Route path="/" element={<h1>Main Page</h1>} />
+            <Route path="/login" element={<h1>Login</h1>} />
+            <Route path="/other" element={<h1>Other</h1>} />
+        </Routes>
     );
 }
