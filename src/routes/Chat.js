c0fc93aa06295c588f563c10b9d91ab5ec3e1d65
Jack Wright <jack.louie.wright@gmail.com>
2022-07-25 18:22:47 +0100

    got subscriptions working

--- src/routes/Chat.js
+++ src/routes/Chat.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { gql } from '@apollo/client'
-import { useSubscription } from '@apollo/client'
+import { useSubscription, useMutation, useQuery } from '@apollo/client'
+import { TextField, Button } from '@mui/material'
 
 const SUBSCRIPTION = gql`
     subscription MySub {
@@ -8,14 +9,67 @@ const SUBSCRIPTION = gql`
     }
 `
 
+const MSG_QUERY = gql`
+    query Q {
+        doNothing
+    }
+`
+
+const SEND_MSG = gql`
+    mutation M($text: String!) {
+        SendMsg(text: $text) {
+            success
+        }
+    }
+`
+
+const MSG_SUBSCRIPTION = gql`
+    subscription RealSub {
+        MsgSub {
+            text
+        }
+    }
+`
+
+class Messages extends React.Component {
+    render() {
+        console.log(this.props)
+        return (
+            <div>
+                <h1>Messages {!this.props.loading && String(this.props.data.doNothing)}</h1>
+            </div>
+        )
+    }
+    componentDidMount() {
+        this.props.doSubscribe()
+    }
+}
+
 export default function Chat() {
+    const { subscribeToMore, ...result } = useQuery(
+        MSG_QUERY, {}
+    )
+    const [SendMut, { }] = useMutation(SEND_MSG)
     const { data, loading } = useSubscription(
-        SUBSCRIPTION,
+        MSG_SUBSCRIPTION,
         {}
     )
     return (
+        // this is terrible, better way?
         <div>
-            <h1>CHAT {data}</h1>
+            <Button onClick={() => { SendMut({ variables: { text: "Hello World" } }) }}>Send Msg</Button>
+            <h1>Last Message: {!loading && data.MsgSub.text}</h1>
+            <Messages {...result} doSubscribe={() => {
+                subscribeToMore({
+                    document: MSG_SUBSCRIPTION,
+                    variables: {},
+                    updateQuery: (prev, { subscriptionData }) => {
+                        console.log("called")
+                        if (!subscriptionData.data) return prev
+                        return { doNothing: "masdkmsalkmdsakmdlkm" }
+                    }
+                })
+            }} />
         </div>
     )
 }
